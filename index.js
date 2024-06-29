6dc4d8b44f7221708d0d229d2f91e1774ed60e11
Miliya Immatty Lazar <miliyaninu@gmail.com>
2024-06-29 13:46:18 +1000

    completed

---
+++ index.js
@@ -0,0 +1,53 @@
+
+function shout(string) {
+    return string.toUpperCase();
+  }
+  const string = "Hello!";
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+function logShout(string)
+{
+    const istring= string.toUpperCase()
+    console.log(istring)
+}
+function logWhisper(string){
+    string= string.toLowerCase();
+    console.log(string)
+
+}
+
+function sayHiToHeadphonedRoommate(string) {
+    if(string == string.toLowerCase())
+    { 
+        return "I can\'t hear you!"
+}
+else if(string == string.toUpperCase())
+{
+    return "YES INDEED!"
+
+}
+else if(string === "Let\'s have dinner together!"){
+    return "Lets have dinner together"
+}}
+   function sayHiToHeadphonedRoommate(string){
+    if(string == string.toLowerCase())
+    {
+        return "I can\'t hear you!"
+    }
+    else if(string == string.toUpperCase()){
+       return "YES INDEED!" 
+    }
+    else if(string === "Let\'s have dinner together!"){
+        return "I would love to!"
+    }
+   }
+    
+    
+    
+
+  
+
+  
+    
